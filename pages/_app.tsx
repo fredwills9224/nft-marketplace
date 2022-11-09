b91af5c69e007328629c6fe69f218261e51a62d6
fredwills9224 <willsf9224@gmail.com>
2022-11-09 15:13:17 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/navbar';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return(
+    <>
+
+      <Navbar/>
+      <Component {...pageProps} />
+    
+    </>
+  ) 
 }
+
+export default App;
